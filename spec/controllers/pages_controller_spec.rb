e7a3161d85acc246b39f8dd71fda17609528be63
Ren Pollock <willpol1@umbc.edu>
2012-02-19 20:59:26 -0500

    updated pages_controller_spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -13,23 +13,13 @@ describe PagesController do
       response.should be_success
     end
   end
-  
-=begin    
+   
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
                                     :content => "Ruby on Rails Tutorial Sample App | Home")
     end
     
-=end    
-  
-  
-#    it "should have the right title" do
-#    get 'help'
-#    response.should have_selector("title",
-#                                  :content => "Ruby on Rails Tutorial Sample App | Home")
-#    end
-#    end
 
   describe "GET 'contact'" do
     it "should be successful" do
