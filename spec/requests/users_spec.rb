eb75c714d8b810a4cbc7c0681902cd83ca5fca04
Ren Pollock <willpol1@umbc.edu>
2012-04-15 21:55:09 -0400

    done with exercise1

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,21 @@
 require 'spec_helper'
 
 describe "Users" do
+  
+RSpec.configure do |config|
+
+def test_sign_in(user)
+  controller.sign_in(user)
+end
+
+def integration_sign_in(user)
+  visit signin_path
+  fill_in :email,	:with => user.email
+  fill_in :password, :with => user.password
+  click_button
+end
+
+=begin
   describe "GET /users" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -8,4 +23,7 @@ describe "Users" do
       response.status.should be(200)
     end
   end
+=end  
+
+end
 end
